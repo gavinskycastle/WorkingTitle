1a9ecaae2d27161d61d1f87343ced21705dc40a7
Gavin P <gavinjpeeler@gmail.com>
2024-01-29 09:23:55 -0800

    Sum more changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 #include "../libs/raylib/src/raylib.h"
 #include "../libs/raygui/src/raygui.h"
 #include "helper.hpp"
+#include "platform.hpp"
+
+Color borderColor;
+Color backgroundColor;
+Color fillColor;
+Color fillLight;
+
+Platform testPlatform;
 
 void init_app() {
-    
+    borderColor = Color{11, 36, 71};
+    backgroundColor = Color{25, 55, 109};
+    fillColor = Color{87, 108, 188};
+    fillLight = Color{165, 215, 232};
 }
 
 bool app_loop() {
-
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground(backgroundColor);
+    
     
     
     EndDrawing();
---
+++ src/platform.hpp
@@ -0,0 +1,3 @@
+enum PlatformRotation;
+
+class Platform;
