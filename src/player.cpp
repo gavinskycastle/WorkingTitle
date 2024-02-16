9cd9e3d35a30198eec8c7f20104aeea50d13ba67
Gavin P <gavinjpeeler@gmail.com>
2024-02-16 10:12:39 -0800

    Started work on player animation

--- src/player.cpp
+++ src/player.cpp
@@ -8,7 +8,7 @@ Player::Player(int x, int y, Color border_color, Color fill_color) {
     this->border_color = border_color;
     this->fill_color = fill_color;
     
-    this->platform = new Platform(x, y, 100, 35, border_color, fill_color, 0, 5, false);
+    this->platform = new Platform(x+16, y+45, 100, 35, border_color, fill_color, 0, 5, false);
 }
 
 void Player::setPosition(int x, int y) {
@@ -23,6 +23,9 @@ void Player::move(int dx, int dy) {
 
 void Player::draw() {
     
+    DrawCircle(x+(100/2)-15, y-(35/2), 10, WHITE);
+    DrawCircle(x+(100/2)+15, y-(35/2), 10, WHITE);
+    
     // Jump key press detection
     if (IsKeyPressed(KEY_W) && this->gravity == 0) {
         this->gravity = -10;
@@ -35,12 +38,20 @@ void Player::draw() {
             this->x -= 10;
         else
             this->x -= 5;
+        DrawCircle(x+(100/2)-15-3, y-(35/2), 5, BLACK);
+        DrawCircle(x+(100/2)+15-3, y-(35/2), 5, BLACK);
     }
-    if (IsKeyDown(KEY_D)) {
+    else if (IsKeyDown(KEY_D)) {
         if (IsKeyDown(KEY_LEFT_SHIFT))
             this->x += 10;
         else
             this->x += 5;
+        DrawCircle(x+(100/2)-15+3, y-(35/2), 5, BLACK);
+        DrawCircle(x+(100/2)+15+3, y-(35/2), 5, BLACK);
+    }
+    else {
+        DrawCircle(x+(100/2)-15, y-(35/2), 5, BLACK);
+        DrawCircle(x+(100/2)+15, y-(35/2), 5, BLACK);
     }
     
     // Gravity decceleartion/acceleration
@@ -60,16 +71,15 @@ void Player::draw() {
         this->gravity = 0;
         
     }
-    
-    DrawCircle(x+(100/2)-15, y-(35/2), 10, WHITE);
-    DrawCircle(x+(100/2)+15, y-(35/2), 10, WHITE);
 
-    DrawCircle(x+(100/2)-15+5, y-(35/2), 5, BLACK);
-    DrawCircle(x+(100/2)+15+5, y-(35/2), 5, BLACK);
+    
 
     DrawLineEx(Vector2{x, y}, Vector2{x-50, y+5}, 7, WHITE);
     DrawLineEx(Vector2{x+100, y}, Vector2{x+100+50, y+10}, 7, WHITE);
     
-    this->platform->setPosition(this->x, this->y);
+    DrawLineEx(Vector2{x+leftLegOrigin.x, y+leftLegOrigin.y}, Vector2{x+35, y+30+50}, 7, WHITE);
+    DrawLineEx(Vector2{x+rightLegOrigin.x, y+rightLegOrigin.y}, Vector2{x+70, y+30+50}, 7, WHITE);
+    
+    this->platform->setPosition(this->x+16, this->y+45);
     this->platform->draw();
 }
--- src/player.hpp
+++ src/player.hpp
@@ -12,6 +12,15 @@ enum PlayerState {
     TIPOVER
 };
 
+struct LimbFrame {
+    Vector2 leftArm;
+    Vector2 rightArm;
+    Vector2 leftLegLimb1;
+    Vector2 leftLegLimb2;
+    Vector2 rightLegLimb1;
+    Vector2 rightLegLimb2;
+};
+
 class Player {
     public:
         Player(int x, int y, Color border_color, Color fill_color);
@@ -25,6 +34,17 @@ class Player {
         float gravity;
         Color border_color;
         Color fill_color;
+        
+        Vector2 leftArmOrigin = Vector2{0, 0};
+        Vector2 rightArmOrigin = Vector2{100, 0};
+        
+        Vector2 leftLegOrigin = Vector2{35, 30};
+        Vector2 rightLegOrigin = Vector2{70, 30};
+        
+        LimbFrame walkFrame1 = LimbFrame{};
+        LimbFrame walkFrame2 = LimbFrame{};
+        LimbFrame walkFrame3 = LimbFrame{};
+        LimbFrame walkFrame4 = LimbFrame{};
         
         Platform* platform;
         PlayerState state;
