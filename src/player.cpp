1067719b47bbf8e1a408ca0de6330a1a5094ce9b
Gavin P <gavinjpeeler@gmail.com>
2024-02-26 11:06:13 -0800

    Added (very janky) walking animation

--- src/player.cpp
+++ src/player.cpp
@@ -31,6 +31,7 @@ void Player::moveToFrame(LimbFrame frame, float seconds) {
 }
 
 void Player::draw() {
+    float dx = 0;
     
     // Draw eyes
     DrawCircle(x+(100/2)-15, y-(35/2), 10, WHITE);
@@ -45,17 +46,17 @@ void Player::draw() {
     // Left and right movement key press detection
     if (IsKeyDown(KEY_A)) {
         if (IsKeyDown(KEY_LEFT_SHIFT))
-            this->x -= 10;
+            dx -= 10;
         else
-            this->x -= 5;
+            dx -= 5;
         DrawCircle(x+(100/2)-15-3, y-(35/2), 5, BLACK);
         DrawCircle(x+(100/2)+15-3, y-(35/2), 5, BLACK);
     }
     else if (IsKeyDown(KEY_D)) {
         if (IsKeyDown(KEY_LEFT_SHIFT))
-            this->x += 10;
+            dx += 10;
         else
-            this->x += 5;
+            dx += 5;
         DrawCircle(x+(100/2)-15+3, y-(35/2), 5, BLACK);
         DrawCircle(x+(100/2)+15+3, y-(35/2), 5, BLACK);
     }
@@ -75,6 +76,9 @@ void Player::draw() {
     // Gravity application
     this->y += this->gravity;
     
+    // Movement application
+    this->x += dx;
+    
     // Ground detection
     if (this->y >= 500) {
         this->y = 500;
@@ -86,14 +90,29 @@ void Player::draw() {
     if (this->timeSinceFrameStart > this->timeToNextFrame) {
         this->frameFinished = true;
     }
+    if (dx == 0) {
+        this->moveToFrame(this->standingFrame, 0.08);
+    } else if (this->finalFrame.id == this->standingFrame.id && this->frameFinished) {
+        this->moveToFrame(this->walkFrame1, 0.08);
+    } else if (this->finalFrame.id == this->walkFrame1.id && this->frameFinished) {
+        this->moveToFrame(this->walkFrame2, 0.08);
+    } else if (this->finalFrame.id == this->walkFrame2.id && this->frameFinished) {
+        this->moveToFrame(this->walkFrame3, 0.08);
+    } else if (this->finalFrame.id == this->walkFrame3.id && this->frameFinished) {
+        this->moveToFrame(this->walkFrame4, 0.08);
+    } else if (this->finalFrame.id == this->walkFrame4.id && this->frameFinished) {
+        this->moveToFrame(this->walkFrame1, 0.08);
+    }
     
     if (this->frameFinished) {
         this->leftArm = finalFrame.leftArm;
         this->rightArm = finalFrame.rightArm;
         this->leftLegLimb1 = finalFrame.leftLegLimb1;
         this->leftLegLimb2 = finalFrame.leftLegLimb2;
+        this->leftLegLimb3 = finalFrame.leftLegLimb3;
         this->rightLegLimb1 = finalFrame.rightLegLimb1;
         this->rightLegLimb2 = finalFrame.rightLegLimb2;
+        this->rightLegLimb3 = finalFrame.rightLegLimb3;
     } else {
         this->timeSinceFrameStart += GetFrameTime();
         
@@ -101,12 +120,10 @@ void Player::draw() {
         this->rightArm = vectorLerp(this->originalFrame.rightArm, finalFrame.rightArm, this->timeSinceFrameStart/this->timeToNextFrame);
         this->leftLegLimb1 = vectorLerp(this->originalFrame.leftLegLimb1, finalFrame.leftLegLimb1, this->timeSinceFrameStart/this->timeToNextFrame);
         this->leftLegLimb2 = vectorLerp(this->originalFrame.leftLegLimb2, finalFrame.leftLegLimb2, this->timeSinceFrameStart/this->timeToNextFrame);
+        this->leftLegLimb3 = vectorLerp(this->originalFrame.leftLegLimb3, finalFrame.leftLegLimb3, this->timeSinceFrameStart/this->timeToNextFrame);
         this->rightLegLimb1 = vectorLerp(this->originalFrame.rightLegLimb1, finalFrame.rightLegLimb1, this->timeSinceFrameStart/this->timeToNextFrame);
         this->rightLegLimb2 = vectorLerp(this->originalFrame.rightLegLimb2, finalFrame.rightLegLimb2, this->timeSinceFrameStart/this->timeToNextFrame);
-    }
-    
-    if (IsKeyPressed(KEY_Z)) {
-        this->moveToFrame(this->testFrame, 0.5);
+        this->rightLegLimb3 = vectorLerp(this->originalFrame.rightLegLimb3, finalFrame.rightLegLimb3, this->timeSinceFrameStart/this->timeToNextFrame);
     }
     
     // Draw limbs
@@ -114,8 +131,10 @@ void Player::draw() {
     DrawLineEx(Vector2{x+rightArmOrigin.x, y+rightArmOrigin.y}, Vector2{x+rightArm.x, y+rightArm.y}, 7, WHITE); // Right arm
     DrawLineEx(Vector2{x+leftLegOrigin.x, y+leftLegOrigin.y}, Vector2{x+leftLegLimb1.x, y+leftLegLimb1.y}, 7, WHITE); // Left leg 1
     DrawLineEx(Vector2{x+leftLegLimb1.x, y+leftLegLimb1.y}, Vector2{x+leftLegLimb2.x, y+leftLegLimb2.y}, 7, WHITE); // Left leg 2
+    DrawLineEx(Vector2{x+leftLegLimb2.x, y+leftLegLimb2.y}, Vector2{x+leftLegLimb3.x, y+leftLegLimb3.y}, 7, WHITE); // Left leg 3
     DrawLineEx(Vector2{x+rightLegOrigin.x, y+rightLegOrigin.y}, Vector2{x+rightLegLimb1.x, y+rightLegLimb1.y}, 7, WHITE); // Right leg 1
     DrawLineEx(Vector2{x+rightLegLimb1.x, y+rightLegLimb1.y}, Vector2{x+rightLegLimb2.x, y+rightLegLimb2.y}, 7, WHITE); // Right leg 2
+    DrawLineEx(Vector2{x+rightLegLimb2.x, y+rightLegLimb2.y}, Vector2{x+rightLegLimb3.x, y+rightLegLimb3.y}, 7, WHITE); // Right leg 3
     
     // Draw platform
     this->platform->setPosition(this->x+16, this->y+45);
--- src/player.hpp
+++ src/player.hpp
@@ -13,12 +13,15 @@ enum PlayerState {
 };
 
 struct LimbFrame {
+    int id;
     Vector2 leftArm;
     Vector2 rightArm;
     Vector2 leftLegLimb1;
     Vector2 leftLegLimb2;
+    Vector2 leftLegLimb3;
     Vector2 rightLegLimb1;
     Vector2 rightLegLimb2;
+    Vector2 rightLegLimb3;
 };
 
 class Player {
@@ -45,27 +48,76 @@ class Player {
         Vector2 rightLegOrigin = Vector2{70, 30};
         
         LimbFrame standingFrame = LimbFrame{
+            0, // ID
             Vector2{-50, 5}, // Left arm
             Vector2{150, 10}, // Right arm
-            Vector2{35, 80}, // Left leg 1
-            Vector2{45, 80}, // Left leg 2
-            Vector2{70, 80}, // Right leg 1
-            Vector2{80, 80}, // Right leg 2
+            Vector2{35, 70}, // Left leg 1
+            Vector2{35, 80}, // Left leg 2
+            Vector2{45, 80}, // Left leg 3
+            Vector2{70, 70}, // Right leg 1
+            Vector2{70, 80}, // Right leg 2
+            Vector2{80, 80}, // Right leg 3
         };
         
         LimbFrame testFrame = LimbFrame{
+            1, // ID
             Vector2{-50-50, 5}, // Left arm
             Vector2{150+50, 10}, // Right arm
-            Vector2{35-50, 80+50}, // Left leg 1
-            Vector2{45-50, 80+50}, // Left leg 2
-            Vector2{70+50, 80+50}, // Right leg 1
-            Vector2{80+50, 80+50}, // Right leg 2
+            Vector2{35-50, 80+40}, // Left leg 1
+            Vector2{35-50, 80+50}, // Left leg 2
+            Vector2{45-50, 80+50}, // Left leg 3
+            Vector2{70+50, 80+40}, // Right leg 1
+            Vector2{70+50, 80+50}, // Right leg 2
+            Vector2{80+50, 80+50}, // Right leg 3
         };
         
-        LimbFrame walkFrame1 = LimbFrame{};
-        LimbFrame walkFrame2 = LimbFrame{};
-        LimbFrame walkFrame3 = LimbFrame{};
-        LimbFrame walkFrame4 = LimbFrame{};
+        LimbFrame walkFrame1 = LimbFrame{
+            2, // ID
+            Vector2{-50, 20}, // Left arm
+            Vector2{150, -4}, // Right arm
+            Vector2{19, 51}, // Left leg 1
+            Vector2{-5, 72}, // Left leg 2
+            Vector2{23, 91}, // Left leg 3
+            Vector2{85, 59}, // Right leg 1
+            Vector2{100, 82}, // Right leg 2
+            Vector2{127, 65}, // Right leg 3
+        };
+        
+        LimbFrame walkFrame2 = LimbFrame{
+            3, // ID
+            Vector2{47, 21}, // Left arm
+            Vector2{144, 25}, // Right arm
+            Vector2{20, 62}, // Left leg 1
+            Vector2{-6, 73}, // Left leg 2
+            Vector2{-1, 94}, // Left leg 3
+            Vector2{101, 48}, // Right leg 1
+            Vector2{91, 94}, // Right leg 2
+            Vector2{120, 88}, // Right leg 3
+        };
+        
+        LimbFrame walkFrame3 = LimbFrame{
+            4, // ID
+            Vector2{-21, 54}, // Left arm
+            Vector2{116, 56}, // Right arm
+            Vector2{42, 62}, // Left leg 1
+            Vector2{42, 92}, // Left leg 2
+            Vector2{66, 94}, // Left leg 3
+            Vector2{70, 58}, // Right leg 1
+            Vector2{17, 66}, // Right leg 2
+            Vector2{18, 82}, // Right leg 3
+        };
+        
+        LimbFrame walkFrame4 = LimbFrame{
+            5, // ID
+            Vector2{-19, 52}, // Left arm
+            Vector2{94, 58}, // Right arm
+            Vector2{31, 55}, // Left leg 1
+            Vector2{22, 75}, // Left leg 2
+            Vector2{44, 89}, // Left leg 3
+            Vector2{92, 55}, // Right leg 1
+            Vector2{71, 72}, // Right leg 2
+            Vector2{80, 92}, // Right leg 3
+        };
         
         // Animation stuff
         void moveToFrame(LimbFrame frame, float seconds);
@@ -81,8 +133,10 @@ class Player {
         Vector2 rightArm;
         Vector2 leftLegLimb1;
         Vector2 leftLegLimb2;
+        Vector2 leftLegLimb3;
         Vector2 rightLegLimb1;
         Vector2 rightLegLimb2;
+        Vector2 rightLegLimb3;
 };
 
 #endif // PLAYER_H
